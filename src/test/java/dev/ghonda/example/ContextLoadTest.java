20b50664dc3ae55b3331d86753b3195fa1218a4f
Gabriel Honda <gabrielhondacba@gmail.com>
2025-08-03 17:59:46 -0400

    test: renomeia classe de teste e ajusta configuração
    - Renomeia a classe `WhyIntegrationTestsSucksTemplateApplicationTests` para `ContextLoadTest` para maior clareza.
    - Substitui a anotação `@Import` para usar a configuração `PostgreSQLContainerConfiguration`.

--- src/test/java/dev/ghonda/example/ContextLoadTest.java
+++ src/test/java/dev/ghonda/example/ContextLoadTest.java
@@ -1,12 +1,13 @@
 package dev.ghonda.example;
 
+import dev.ghonda.example.configuration.PostgreSQLContainerConfiguration;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
 
-@Import(TestcontainersConfiguration.class)
 @SpringBootTest
-class WhyIntegrationTestsSucksTemplateApplicationTests {
+@Import(PostgreSQLContainerConfiguration.class)
+class ContextLoadTest {
 
     @Test
     void contextLoads() {
